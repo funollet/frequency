f9e191273700895b3f7cd89849a46ab2801dff0b
Jordi Funollet <funollet@fastmail.fm>
2014-03-27 23:06:56 +0100

    Refactored into a class Pwm().

--- freq.py
+++ freq.py
@@ -10,40 +10,42 @@ import atexit
 SECONDS_UP = 0.5
 DEBUG = True
 
-sched = Scheduler(daemon=True)
 
 def log(msg):
     if DEBUG:
         print '%s: %s' % (datetime.now(), msg)
 
 
-def ding():
-    log("ding")
-    sched.add_date_job(dong, datetime.now() + timedelta(0, SECONDS_UP, 0))
+class Pwm(object):
 
-def dong():
-    log("dong")
+    def __init__(self, pin, period, duration):
+        self.pin = pin
+        self.period = period
+        self.duration = duration
+        # Call on() in a new thread.
+        sched.add_interval_job(self.__on, seconds=self.period)
 
+    def __on(self):
+        log('%s On' % self.pin)
+        off_time = datetime.now() + timedelta(0, self.duration, 0)
+        # Schedule off() to run, in another thread, after 'off_time'.
+        sched.add_date_job(self.__off, off_time)
 
-def tilin():
-    log("   tilin")
-    sched.add_date_job(tolon, datetime.now() + timedelta(0, SECONDS_UP, 0))
-
-def tolon():
-    log("   tolon")
+    def __off(self):
+        log('%s Off' % self.pin)
 
 
 def main():
-
-    atexit.register(lambda: sched.shutdown(wait=False))
-
-    sched.add_interval_job(ding, seconds=1)
-    sched.add_interval_job(tilin, seconds=3)
+    Pwm("ding  ", 1, SECONDS_UP)
+    Pwm("  DONG", 3, SECONDS_UP)
     sched.start()
     while True:
         pass
 
 
 if __name__ == '__main__':
+    sched = Scheduler(daemon=True)
+    atexit.register(lambda: sched.shutdown(wait=False))
+
     main()
 
